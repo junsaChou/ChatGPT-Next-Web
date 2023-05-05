180ec1bb37eb3352586198202eb41dfec84b61b6
junsa <junsa163@163.com>
2023-05-05 18:57:54 +0800

    up process

--- app/store/access.ts
+++ app/store/access.ts
@@ -2,7 +2,7 @@
  * @Author: junsa junsa163@163.com
  * @Date: 2023-05-05 14:32:13
  * @LastEditors: junsa junsa163@163.com
- * @LastEditTime: 2023-05-05 18:39:22
+ * @LastEditTime: 2023-05-05 18:57:34
  * @FilePath: /workerWebsite/githup/ChatGPT-Next-Web/app/store/access.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -10,6 +10,21 @@ import { create } from "zustand";
 import { persist } from "zustand/middleware";
 import { StoreKey } from "../constant";
 import { BOT_HELLO } from "./chat";
+
+export function getEnvVar(varName: any) {
+  // 如果在 Node.js 环境中，则从 process.env 中读取变量的值
+  if (typeof process !== "undefined" && process.env && process.env[varName]) {
+    return process.env[varName];
+  }
+
+  // 如果在浏览器环境中，则从 window 对象中读取变量的值
+  if (typeof window !== "undefined" && window[varName]) {
+    return window[varName];
+  }
+
+  // 如果未定义该变量，则返回 undefined
+  return undefined;
+}
 
 export interface AccessControlStore {
   accessCode: string;
@@ -31,7 +46,7 @@ let fetchState = 0; // 0 not fetch, 1 fetching, 2 done
 export const useAccessStore = create<AccessControlStore>()(
   persist(
     (set, get) => ({
-      token: process.env.OEPNAI_API_KEY,
+      token: getEnvVar("OPENAI_API_KEY"),
       accessCode: "",
       needCode: true,
       hideUserApiKey: true, //false,
