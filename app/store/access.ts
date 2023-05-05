06ad9c44a82fa89d23e552964543793a5f8ba21c
junsa <junsa163@163.com>
2023-05-05 18:41:23 +0800

    up apikey

--- app/store/access.ts
+++ app/store/access.ts
@@ -2,7 +2,7 @@
  * @Author: junsa junsa163@163.com
  * @Date: 2023-05-05 14:32:13
  * @LastEditors: junsa junsa163@163.com
- * @LastEditTime: 2023-05-05 17:54:52
+ * @LastEditTime: 2023-05-05 18:39:22
  * @FilePath: /workerWebsite/githup/ChatGPT-Next-Web/app/store/access.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -13,7 +13,7 @@ import { BOT_HELLO } from "./chat";
 
 export interface AccessControlStore {
   accessCode: string;
-  token: string;
+  token: any;
 
   needCode: boolean;
   hideUserApiKey: boolean;
@@ -31,7 +31,7 @@ let fetchState = 0; // 0 not fetch, 1 fetching, 2 done
 export const useAccessStore = create<AccessControlStore>()(
   persist(
     (set, get) => ({
-      token: "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",
+      token: process.env.OEPNAI_API_KEY,
       accessCode: "",
       needCode: true,
       hideUserApiKey: true, //false,
