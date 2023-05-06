04a08aff26c3475ac9900e92cbec9c64d6486a2b
junsa <junsa163@163.com>
2023-05-06 14:46:40 +0800

    up .env

--- app/store/access.ts
+++ app/store/access.ts
@@ -2,7 +2,7 @@
  * @Author: junsa junsa163@163.com
  * @Date: 2023-05-05 14:32:13
  * @LastEditors: junsa junsa163@163.com
- * @LastEditTime: 2023-05-05 18:57:34
+ * @LastEditTime: 2023-05-06 14:45:25
  * @FilePath: /workerWebsite/githup/ChatGPT-Next-Web/app/store/access.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -10,22 +10,7 @@ import { create } from "zustand";
 import { persist } from "zustand/middleware";
 import { StoreKey } from "../constant";
 import { BOT_HELLO } from "./chat";
-
-export function getEnvVar(varName: any) {
-  // 如果在 Node.js 环境中，则从 process.env 中读取变量的值
-  if (typeof process !== "undefined" && process.env && process.env[varName]) {
-    return process.env[varName];
-  }
-
-  // 如果在浏览器环境中，则从 window 对象中读取变量的值
-  if (typeof window !== "undefined" && window[varName]) {
-    return window[varName];
-  }
-
-  // 如果未定义该变量，则返回 undefined
-  return undefined;
-}
-
+import { loadEnvConfig } from "@next/env";
 export interface AccessControlStore {
   accessCode: string;
   token: any;
@@ -46,7 +31,7 @@ let fetchState = 0; // 0 not fetch, 1 fetching, 2 done
 export const useAccessStore = create<AccessControlStore>()(
   persist(
     (set, get) => ({
-      token: getEnvVar("OPENAI_API_KEY"),
+      token: process.env.OPENAI_API_KEY,
       accessCode: "",
       needCode: true,
       hideUserApiKey: true, //false,
@@ -64,6 +49,7 @@ export const useAccessStore = create<AccessControlStore>()(
         set(() => ({ token }));
       },
       isAuthorized() {
+        console.log("运行环境变量", process.env.NEXT_PUBLIC_OPENAI_API_KEY);
         // has token or has code or disabled access contro
         return (
           !!get().token || !!get().accessCode || !get().enabledAccessControl()
